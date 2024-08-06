1e6636403be2dd2bc779359590db4f4ec4699341
chapman91 <chapmandoneil91@gmail.com>
2024-08-05 23:35:19 -0400

    -headlessUI -tailwind css forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms'), require('@headlessui/tailwindcss')],
 }
 
+// `**/*` glob pattern that matches zero or more directories ans subdirectories
+// {js,ts,jsx,tsx} is a brace expansion pattern that matches any listed file extensions
+// import css two tailwind css plugins
