bd57da573e4f007b452eacd15969c3915d86a682
chapman91 <chapmandoneil91@gmail.com>
2024-08-06 16:34:09 -0400

    configure project to work with .js and .jsx file formats

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 
 
 import './App.css'
+import {Navbar} from "./components/organisms/Navbar";
 
 function App() {
 
 
   return (
     <>
+    <Navbar/>
     <div className="text-xm">Tailwind css setup for React</div>
     </>
   )
