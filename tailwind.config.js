420854d663d87891f83f1959656765f49763e3b8
chapman91 <chapmandoneil91@gmail.com>
2024-08-11 12:45:26 -0400

    restructure the prject directory to vanilla structure -simplifies the process of mastering tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ['*.{html,js}'],
   theme: {
-    extend: {
-      colors: {
-        Primary: "#40B750",
-        OnPrimary: "#EFD59E",
-        PrimaryContainer: "#85D08F",
-        OnPrimaryContainer: "#F9EED8",
-        Secondary: "#0E0E0E",
-        SecondaryContainer: "#CDE1CF"
-      }
-    },
+    extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [],
 }
 
