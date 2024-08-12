e8644a87a4aadaa0f30d78edf5d2d6b7e94e882a
chapman91 <chapmandoneil91@gmail.com>
2024-08-12 04:19:47 -0400

    Add Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     container: {
       padding: {
-        DEFAULT: '15px'
+        DEFAULT: '15px',
       },
     },
     screens: {
@@ -15,39 +15,36 @@ module.exports = {
     },
     fontFamily: {
       primary: 'DM Serif Display',
-      secondary: 'Jost', 
+      secondary: 'Jost',
     },
     backgroundImage: {
       hero: 'url(/assets/hero/Background.png)',
     },
     extend: {
       colors: {
-        first: { 
-        primary: '#40B750',
-        primaryLight: 'rgba(249, 238, 216, 0.3)',
-        onPrimary: '#EFD59E',
-        primaryContainer: '#85D08F',
-        onPrimaryContainer: '#F9EED8',
-        customGradient: 'linear-gradient(to right, #85D08F 17%, #85D08F 31%, #EFD59E 46%)',
-        hover: '#E0E0E0',
-      },
+        first: {
+          primary: '#40B750',
+          primaryLight: 'rgba(249, 238, 216, 0.3)',
+          onPrimary: '#EFD59E',
+          primaryContainer: '#85D08F',
+          onPrimaryContainer: '#F9EED8',
+          customGradient:
+            'linear-gradient(to right, #85D08F 17%, #85D08F 31%, #EFD59E 46%)',
+          hover: '#E0E0E0',
+        },
         second: {
           secondary: '#0E0E0E',
           onSecondary: '#FEFEFE',
           secondaryContainer: '#CDE1CF',
           onSecondaryContainer: '#D9D9D9',
         },
-      accent: {
-        default: '#FF5733',  // Default accent color
-        secondary: '#33CFFF', // Secondary accent color
-        hover: '#E0E0E0',
-      }
-    
-      }
+        accent: {
+          default: '#FF5733', // Default accent color
+          secondary: '#33CFFF', // Secondary accent color
+          hover: '#E0E0E0',
+        },
+      },
     },
   },
-  plugins: [
-   
-  ],
-}
-
+  plugins: [],
+};
