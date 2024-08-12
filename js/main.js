60ef2fea8dc8eb19c04a84259715976c8e0a5c82
chapman91 <chapmandoneil91@gmail.com>
2024-08-12 13:49:18 -0400

    frequently asked question added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 const navTriggerBtn = document.querySelector('#nav_trigger_btn');
 const navMenu = document.querySelector('#nav_menu');
+const accordionHeader = document.querySelectorAll('.accordion-header');
 
 // event listener
 navTriggerBtn.addEventListener('click', () => {
   navMenu.classList.toggle('nav-is-open');
+});
+
+accordionHeader.forEach((accordionHeader) => {
+  accordionHeader.addEventListener('click', (event) => {
+    accordionHeader.classList.toggle('active');
+    const accordionBody = accordionHeader.nextElementSibling;
+    if (accordionHeader.classList.contains('active')) {
+      accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
+    } else {
+      accordionBody.style.maxHeight = 0;
+    }
+  });
 });
