b9b85c1c4ea082b5feec3e1064741e0b1d733179
chapman91 <chapmandoneil91@gmail.com>
2024-08-13 04:07:08 -0400

    add animation

--- js/main.js
+++ js/main.js
@@ -18,3 +18,46 @@ accordionHeader.forEach((accordionHeader) => {
     }
   });
 });
+
+// Swiper or similar slider setup
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 20,
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    960: {
+      slidesPerView: 2, // Fixed typo
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// Scroll reveal animations
+const sr = ScrollReveal({
+  origin: 'bottom',
+  distance: '60px',
+  duration: 3000,
+  delay: 600,
+  // Uncomment if you want the animation to reset on scroll
+});
+
+// Specific reveal settings
+sr.reveal('.hero__text', { origin: 'top' });
+
+// steps
+sr.reveal('.steps__step', { distance: '100px', interval: 100 });
+
+// about text & image
+sr.reveal('.about__text', { origin: 'left' });
+sr.reveal('.about__img', { origin: 'right', delay: 800 });
+
+// faqs
+sr.reveal('.accordion__section', { delay: 800 });
+sr.reveal('.accordion__title', { delay: 800 });
+
+// contact form
+sr.reveal('.contact__form', { delay: 800 });
