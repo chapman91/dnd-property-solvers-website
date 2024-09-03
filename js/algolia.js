8957e414695bbf6d2a70d8014e29cdde7b91675a
chapman91 <chapmandoneil91@gmail.com>
2024-09-02 21:03:33 -0400

    Add module type to the dynamically created script

--- js/algolia.js
+++ js/algolia.js
@@ -12,6 +12,7 @@ const script = document.createElement('script');
 script.src =  `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
 script.async = true;
 script.defer = true;
+script.type  = 'module'; 
 document.head.appendChild(script);
 
 }
